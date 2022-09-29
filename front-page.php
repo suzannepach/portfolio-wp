807b433406af99c8c59afe0fe00f7b39872ce452
Suzanne-Pach <suzanne@suzannepach.com>
2022-09-29 11:01:02 +0200

    Added recent projects section to homepage

--- front-page.php
+++ front-page.php
@@ -9,11 +9,37 @@
 
 get_header();
 ?>
-	<main id="primary" class="site-main">
-		<?php
-				get_template_part( 'template-parts/content', 'page' );
-		?>
-	</main><!-- #main -->
+<main id="primary" class="site-main">
+	<?php
+			get_template_part( 'template-parts/content', 'page' );
+	?>
+</main><!-- #main -->
+
+<!-- RECENT PROJECTS -->
+<section class="featured-work">
+	<div class="site-content clearfix">
+		<h4>Recent Work</h4>
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=projects'); ?>
+		<!-- the loop -->
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<a  href="<?php the_permalink(); ?>">
+					<figure>	
+						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
+			</li>
+			<?php endwhile; // end of the loop. ?> 
+		<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</ul>
+	</div>
+</section>
+
 <?php
 get_footer();
 ?>
--- single-projects.php
+++ single-projects.php
@@ -13,61 +13,60 @@ get_header();
 <main id="primary" class="site-main site-content">
 	<!-- The case studies form had custom fields added using the plugin Advanced Cusatom Fields. 
 	The code in the loop here relates to those custom fields (see SK wordpress 203 lesson 8 step 5). -->
-	<div id="primary" class="site-content">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); 
-				$skills_used = get_field('skills_used');
-				$project_link = get_field('project_link');
-				$github_link = get_field('github_link');
-				$description_image_1 = get_field('description_image_1');
-				$image_1 = get_field('image_1');
-				$description_image_2 = get_field('description_image_2');
-				$image_2 = get_field('image_2');
-				$description_image_3 = get_field('description_image_3');
-				$image_3 = get_field('image_3');
-				$size = "full";
-			?>
-				<article class="case-study clearfix">
-					<aside class="case-study-sidebar">
-						<h2><?php the_title(); ?></h2> 
-						<h4><span><?php echo $skills_used; ?></span></h4>
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post(); 
+			$skills_used = get_field('skills_used');
+			$project_link = get_field('project_link');
+			$github_link = get_field('github_link');
+			$description_image_1 = get_field('description_image_1');
+			$image_1 = get_field('image_1');
+			$description_image_2 = get_field('description_image_2');
+			$image_2 = get_field('image_2');
+			$description_image_3 = get_field('description_image_3');
+			$image_3 = get_field('image_3');
+			$size = "full";
+		?>
+			<article class="case-study clearfix">
+				<aside class="case-study-sidebar">
+					<h2><?php the_title(); ?></h2> 	
+					<h4><span><?php echo $skills_used; ?></span></h4>
 
-						<?php the_content(); ?>
+					<?php the_content(); ?>
 
-						<p><a href="<?php echo $project_link; ?>" class="read-more-link">Visit Live Site &rsaquo;</a></p>
-						<p><a href="<?php echo $github_link; ?>" class="read-more-link">See Code On GitHub &rsaquo;</a></p>
-					</aside>
-					<div class="case-study-images">
-						<?php if($description_image_1) { ?>
-							<p><?php echo $description_image_1; ?></p> 
-						<?php }
-						if($image_1) { 
-							echo wp_get_attachment_image( $image_1, $size );
-						} 
-						if($description_image_2) {  ?>
-							<p><?php echo $description_image_2; ?></p> 
-						<?php }
-						if($image_2) {
-							echo wp_get_attachment_image( $image_2, $size );
-						}
-						if($description_image_3) {  ?>
-							<p><?php echo $description_image_3; ?></p>
-						<?php }
-						if($image_3) {
-							echo wp_get_attachment_image( $image_3, $size );
-						} ?>
-					</div>
-				</article>	
+					<p><a href="<?php echo $project_link; ?>" class="read-more-link">Visit Live Site &rsaquo;</a></p>
+					<p><a href="<?php echo $github_link; ?>" class="read-more-link">See Code On GitHub &rsaquo;</a></p>
+				</aside>
+				<div class="case-study-images">
+					<?php if($description_image_1) { ?>
+						<p><?php echo $description_image_1; ?></p> 
+					<?php }
+					if($image_1) { 
+						echo wp_get_attachment_image( $image_1, $size );
+					} 
+					if($description_image_2) {  ?>
+						<p><?php echo $description_image_2; ?></p> 
+					<?php }
+					if($image_2) {
+						echo wp_get_attachment_image( $image_2, $size );
+					}
+					if($description_image_3) {  ?>
+						<p><?php echo $description_image_3; ?></p>
+					<?php }
+					if($image_3) {
+						echo wp_get_attachment_image( $image_3, $size );
+					} ?>
+				</div>
+			</article>	
 
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
 
-	</div><!-- #primary -->
+</main><!-- #main -->	
 
-	<nav id="navigation" class="container">
-		<div class="left"><a href="<?php echo site_url('/projects/') ?>">&larr; <span>Back to Projects</span></a></div>
-	</nav>
-</main><!-- #main -->
+<nav id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/projects/') ?>">&larr; <span>Back to Projects</span></a></div>
+</nav>
+
 
 <?php
 get_footer();
