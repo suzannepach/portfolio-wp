dcc4867aff4cfb9cae7caf9f19d2cbc7f81d0fcb
Suzanne-Pach <suzanne@suzannepach.com>
2023-02-08 16:23:17 +0100

    Size images front page changed from medium to default

--- front-page.php
+++ front-page.php
@@ -24,7 +24,7 @@ get_header();
 		<!-- the loop -->
 			<?php while ( have_posts() ) : the_post(); 
 				$image_1 = get_field("image_1");
-				// $size = "medium";
+				$size = "default";
 				$skills_used = get_field('skills_used');
 			?>
 			<li class="individual-project">
